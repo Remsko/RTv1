c1d30c04c2297682f684fad57f4cfb698e8a1c3a
Alexis DA-CUNHA <ada-cunh@e2r9p11.42.fr>
2018-01-24 16:05:33 +0100

    Add norm_obj, add some calc funct

--- incs/vector_utility.h
+++ incs/vector_utility.h
@@ -6,7 +6,7 @@
 /*   By: nboste <nboste@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/31 20:17:46 by nboste            #+#    #+#             */
-/*   Updated: 2018/01/24 14:09:23 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/01/24 16:01:37 by ada-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ t_point vector_sub(t_point p1, t_point p2);
 
 void	normalize_vector(t_point *p);
 
-double	get_norm(t_point p);
+// double	get_norm(t_point p);
 
-float 	get_angle(t_point a, t_point b);
+double	vect_len(t_point p);
 
-double	calc_dist(t_point a, t_point b);
+double 	get_angle(t_point a, t_point b);
 
 double  vec_dist(t_point p1, t_point p2);
 
