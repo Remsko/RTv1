a42a4959a73d5b37e3c9383748705a74bff44eeb
Remi PINOIT <rpinoit@e2r10p12.42.fr>
2018-01-29 11:54:18 +0100

    no working light

--- srcs/raytracer.c
+++ srcs/raytracer.c
@@ -6,7 +6,7 @@
 /*   By: ada-cunh <ada-cunh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/26 12:47:44 by ada-cunh          #+#    #+#             */
-/*   Updated: 2018/01/29 11:29:55 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/01/29 11:53:43 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ t_color			raytrace(t_ray r, t_env *env)
 	t_intersection     inter;
 	t_tmp		tmp;
 
-	tmp.c = (t_color){109, 109, 109, 1};
+	tmp.c = (t_color){109 / 255, 109 / 255, 109 / 255, 1};
 	tmp.obj = env->scene.objs;
 	inter.t = MAX_RAY_LENGTH;
 	if (intersection(r, tmp.obj, &inter))
@@ -73,8 +73,8 @@ t_color			raytrace(t_ray r, t_env *env)
 //			printf("cylindre\n");
 //		if (inter.obj->type == plan)
 //			printf("plan\n");
-		tmp.c = inter.obj->color;
+		tmp.c = process_light(env->scene.lgts, tmp.obj, inter.obj, inter.pos);
 	}
-//	return (get_final_color(tmp.c));
-	return (tmp.c);
+	return (get_final_color(tmp.c));
+//	return (tmp.c);
 }
