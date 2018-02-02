244822e3658b0bd0f61277e2c9aca2a92c7e7cab
Cwocco <cwo.payp@gmail.com>
2018-02-02 14:38:35 +0100

    Vector rotate

--- srcs/vector_rotate.c
+++ srcs/vector_rotate.c
@@ -13,7 +13,14 @@
 #include "vector_rotate.h"
 #include <math.h>
 
-void rotate_x(t_point *vec, double angle)
+void set_rotate(t_point rotate)
+{
+  rotate.x = 0;
+  rotate.y = 0;
+  rotate.z = 0;
+}
+
+static void rotate_x(t_point *vec, double angle)
 {
 	double	tmp_y;
 	double	tmp_z;
@@ -24,7 +31,7 @@ void rotate_x(t_point *vec, double angle)
 	vec->z = tmp_y * sin(angle) + tmp_z * cos(angle);
 }
 
-void rotate_y(t_point *vec, double angle)
+static void rotate_y(t_point *vec, double angle)
 {
 	double	tmp_x;
 	double	tmp_z;
@@ -35,7 +42,7 @@ void rotate_y(t_point *vec, double angle)
 	vec->z = tmp_x * sin(angle) + tmp_z * cos(angle);
 }
 
-void rotate_z(t_point *vec, double angle)
+static void rotate_z(t_point *vec, double angle)
 {
     double  tmp_x;
     double  tmp_y;
@@ -46,9 +53,9 @@ void rotate_z(t_point *vec, double angle)
     vec->z = tmp_x * sin(angle) + tmp_y * cos(angle);
 }
 
-void rotate_vec(t_point *vec, double angle)
+void rotate_vec(t_point *vec, t_point rotate)
 {
-	rotate_x();
-	rotate_y();
-	rotate_z();
+  rotate_x(vec, ft_degtorad(rotate.x));
+  rotate_y(vec, ft_degotrad(rotate.y));
+  rotate_z(vec, ft_degtorad(rotate.z));
 }
